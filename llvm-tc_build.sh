fed0f0acccd8371bc2223e8364e974dfa1e03d04
Angga R <nggdclxvi@protonmail.com>
2021-10-31 16:51:57 +0700

    Update llvm-tc_build.sh

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc
+git clone https://github.com/AnggaR96s/tc-build $(pwd)/llvmTC
 cd $(pwd)/llvmTC
 bash build-tc.sh
